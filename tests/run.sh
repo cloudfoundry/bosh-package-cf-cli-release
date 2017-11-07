2e3ac1ac14f5fc4adf9bb432041f76c81804199f
Anand Gaitonde <agaitonde@pivotal.io>
2017-11-07 10:39:48 -0800

    updated bosh release to support vendoring and job collocation
    - deleted 32 bit linux CLI because we only have 64 bit ubuntu stemcells
    - renamed our 64 bit distribution
    - deleted existing final releases
    - created a job that can be collocated with other bosh jobs
    - created a bosh errand job (test job)

---
+++ tests/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+export BOSH_DEPLOYMENT=cf-cli-test
+export BOSH_NON_INTERACTIVE=true
+
+echo "-----> `date`: Delete previous deployment"
+bosh delete-deployment --force
+
+echo "-----> `date`: Deploy"
+bosh deploy ./manifests/test.yml
+
+echo "-----> `date`: Run test errand"
+bosh run-errand cf-cli-6-linux-test
+
+echo "-----> `date`: Delete deployments"
+bosh delete-deployment
+
+echo "-----> `date`: Done"
