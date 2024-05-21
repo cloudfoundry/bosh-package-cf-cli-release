35bc1d4ca10a0f82df1af37237c15692e40c193a
Michael Chinigo <michael.chinigo@broadcom.com>
2024-05-21 17:28:07 +0000

    WIP - Try pushing to github from cloud

--- ci/scripts/create-bosh-release-candidate.sh
+++ ci/scripts/create-bosh-release-candidate.sh
@@ -19,12 +19,12 @@ OLD_BLOB_PATH=$(bosh blobs --column=path | grep "${prefix:?}")
 OLD_CLI_VERSION=$(echo "${OLD_BLOB_PATH:?}" | cut -d_ -f2)
 
 if [[ "${OLD_CLI_VERSION:?}" != "${LATEST_CLI_VERSION:?}" ]]; then
-    git config user.email "${GITHUB_ACTOR}+github-actions[bot]@users.noreply.github.com"
-    git config user.name "github-actions[bot]"
+    # git config user.email "${GITHUB_ACTOR}+github-actions[bot]@users.noreply.github.com"
+    # git config user.name "github-actions[bot]"
 
-    echo "foo: ${GITHUB_ACTOR}, ${GITHUB_TOKEN}"
+    # echo "foo: ${GITHUB_ACTOR}, ${GITHUB_TOKEN}"
 
-    git remote set-url --push origin "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+    # git remote set-url --push origin "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
     bosh remove-blob "${OLD_BLOB_PATH}"
 
@@ -34,7 +34,8 @@ if [[ "${OLD_CLI_VERSION:?}" != "${LATEST_CLI_VERSION:?}" ]]; then
     git add config/blobs.yml
     git status
     git commit -m "bump v8 cli to ${LATEST_CLI_VERSION}"
-    git push origin HEAD:foo
+    git push
+    echo 'successfully pushed'
 else
     echo "Release has latest v8 CLI version, skipping bump."
 fi
