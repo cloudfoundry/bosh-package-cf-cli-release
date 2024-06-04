ba4bfbfae559d885c4583b8cd85dedcd38201938
Michael Chinigo <michael.chinigo@broadcom.com>
2024-06-04 16:09:27 +0000

    WIP - Make git commits as blobs are changed

--- ci/scripts/create-bosh-release-candidate.sh
+++ ci/scripts/create-bosh-release-candidate.sh
@@ -7,6 +7,63 @@ set -o errexit -o nounset -o pipefail
 # shellcheck source=/dev/null
 . "$(dirname "${BASH_SOURCE[0]}")"/stdlib.sh
 
+# Used to find downloaded tarballs and extract semver
+tarball_regex_sed="^.*cf[[:digit:]]\?-cli_\([[:digit:]]\+\).\([[:digit:]\+]\).\([[:digit:]]\+\)_linux_x86-64.tgz$"
+
+diff_and_commit_with_message() {
+  _message=${1}
+
+  git diff --patch config/blobs.yml
+  git add config/blobs.yml
+  git commit --message "${_message}"
+  git log --pretty=full --max-count=1
+}
+
+add_and_commit_blob() {
+  _downloaded_tarball=${1}
+  _downloaded_tarball_basename=$(basename "${_downloaded_tarball}")
+
+  # shellcheck disable=SC2001
+  _major_version=$(echo "${_downloaded_tarball_basename}" | sed "s/${tarball_regex_sed}/\1/")
+  # shellcheck disable=SC2001
+  _full_version=$(echo "${_downloaded_tarball_basename}" | sed "s/${tarball_regex_sed}/\1.\2.\3/")
+
+  echo "::group::Adding blob for v${_major_version} - ${_downloaded_tarball_basename}"
+  bosh add-blob "${_downloaded_tarball}" "${_downloaded_tarball_basename}"
+  diff_and_commit_with_message "Setting CF CLI v${_major_version} to ${_full_version}"
+  echo "::endgroup::"
+}
+
+remove_and_commit_blob() {
+  _published_blob_name=${1}
+
+  # shellcheck disable=SC2001
+  _major_version=$(echo "${_published_blob_name}" | sed "s/${tarball_regex_sed}/\1/")
+
+  echo "::group::Removing blob for v${_major_version}"
+  bosh remove-blob "${_published_blob_name}"
+  diff_and_commit_with_message "Removing CF CLI v${_major_version}"
+  echo "::endgroup::"
+}
+
+update_and_commit_blob() {
+  _published_blob_name=${1}
+  _downloaded_tarball=${2}
+
+  _downloaded_tarball_basename=$(basename "${_downloaded_tarball}")
+  # shellcheck disable=SC2001
+  _major_version=$(echo "${_downloaded_tarball_basename}" | sed "s/${tarball_regex_sed}/\1/")
+  # shellcheck disable=SC2001
+  _published_version=$(echo "${_published_blob_name}" | sed "s/${tarball_regex_sed}/\1.\2.\3/")
+  # shellcheck disable=SC2001
+  _new_version=$(echo "${_downloaded_tarball_basename}" | sed "s/${tarball_regex_sed}/\1.\2.\3/")
+
+  echo "::group::Adding blob for v${_major_version} - ${_downloaded_tarball_basename}"
+  bosh add-blob "${_downloaded_tarball}" "${_downloaded_tarball_basename}"
+  diff_and_commit_with_message "Updating CF CLI v${_major_version} from ${_published_version} to ${_new_version}"
+  echo "::endgroup::"
+}
+
 create_bosh_release_candidate() {
   ## Parse and validate arguments
   while [ ${#} -gt 0 ] ; do
@@ -16,6 +73,16 @@ create_bosh_release_candidate() {
           "") fail_with "Must provide value for --downloaded-binaries-dir";;
           *) _downloaded_binaries_dir="${2}"; shift 2;;
         esac;;
+      --git-email)
+        case "${2:-}" in
+          "") fail_with "Must provide value for --git-email";;
+          *) _git_email="${2}"; shift 2;;
+        esac;;
+      --git-username)
+        case "${2:-}" in
+          "") fail_with "Must provide value for --git-username";;
+          *) _git_username="${2}"; shift 2;;
+        esac;;
       (-*)
         fail_with "Unrecognized option ${1}";;
       (*)
@@ -23,10 +90,12 @@ create_bosh_release_candidate() {
     esac
   done
 
-  [[ -z "${_downloaded_binaries_dir:-}" ]] && fail_with "Must provide --downloaded-binaries-dir"
-
   # Start off assuming no updates
   _blobs_updated=false
+
+  # Set git identity
+  git config user.name  "${_git_username}"
+  git config user.email "${_git_email}"
 
   # Remember current blobs
   echo "::group::Blobs in most recent Bosh release:"
@@ -34,8 +103,8 @@ create_bosh_release_candidate() {
   _published_blobs=$(bosh blobs --json | jq --compact-output '.Tables[0].Rows[] | {path, digest}')
   echo "::endgroup::"
 
-  ## STEP 1: Prune mismatched blobs from current Bosh release
-  echo "Removing blobs from Bosh release that do not match downloaded binaries."
+  ## STEP 1: Prune or replace mismatched blobs from current Bosh release
+  echo "Replacing blobs from Bosh release that do not match downloaded binaries."
 
   for _published_blob in ${_published_blobs}; do
     _published_blob_name=$(echo "${_published_blob}" | jq --raw-output '.path')
@@ -46,14 +115,14 @@ create_bosh_release_candidate() {
       _blobs_updated=true
 
       echo "Published blob ${_published_blob_name} does not have corresponding downloaded binary. Removing from new release."
-      bosh remove-blob "${_published_blob_name}"
+      remove_and_commit_blob  "${_published_blob_name}"
 
     # Found named tarball - Compare digests
     else
       _published_blob_digest=$(echo "${_published_blob}" | jq --raw-output '.digest')
       _downloaded_tarball_digest="sha256:$(sha256sum "${_resolved_tarball}" | cut --delimiter ' ' --field 1)"
 
-      # Digest mismatch - Prune
+      # Digest mismatch - Update
       if [[ "${_published_blob_digest}" != "${_downloaded_tarball_digest}" ]]; then
         _blobs_updated=true
 
@@ -74,36 +143,20 @@ create_bosh_release_candidate() {
   echo "Adding downloaded binaries to the Bosh release."
 
   # Find tarballs
-  tarball_regex="^.*/cf[0-9]?-cli_([0-9]+\.[0-9]+\.[0-9]+)_linux_x86-64\.tgz$" \
   _downloaded_tarballs=$(find "${_downloaded_binaries_dir}"/* \
     -type f \
-    -regextype posix-extended \
-    -regex "${tarball_regex}")
+    -regextype sed \
+    -regex "${tarball_regex_sed}")
 
   for _downloaded_tarball in ${_downloaded_tarballs}; do
     _downloaded_tarball_basename=$(basename "${_downloaded_tarball}")
-    _downloaded_tarball_major_version=$(basename "$(dirname "${_downloaded_tarball}")")
     _published_blob=$(echo "${_updated_published_blobs}" | jq ". | select(.path == \"${_downloaded_tarball_basename}\")")
 
     if [[ -z "${_published_blob}" ]]; then
       _blobs_updated=true
 
       echo "Downloaded binary ${_downloaded_tarball_basename} has no corresponding blob in published Bosh release. Adding to new release."
-      bosh add-blob "${_downloaded_tarball}" "${_downloaded_tarball_basename}"
-
-    else
-      _published_tarball_digest=$(echo "${_published_blob}" | jq --raw-output '.digest')
-      _downloaded_tarball_digest="sha256:$(sha256sum "${_downloaded_tarball}" | cut --delimiter ' ' --field 1)"
-
-      if [[ "${_published_tarball_digest}" != "${_downloaded_tarball_digest}" ]]; then
-        _blobs_updated=true
-
-        echo "Downloaded binary ${_downloaded_tarball} does not match blob in published Bosh release (published release specifies digest ${_published_tarball_digest}, downloaded binary has digest ${_downloaded_tarball_digest}). Adding to new release."
-
-        bosh add-blob "${_downloaded_tarball}" "${_downloaded_tarball_basename}"
-      else
-        echo "Downloaded binary ${_downloaded_tarball_basename} has same digest as blob in published release: ${_published_tarball_digest}. Disregarding."
-      fi
+      add_and_commit_blob "${_downloaded_tarball}"
     fi
   done
 
